af39fc1aa296830600a5a73774294cc06ebb9cb7
alexv767 <alexveli@mail.ru>
2018-09-07 23:08:16 +0300

    Open new windows in browser (final).

---
+++ src/test/java/ru/stqa/training/selenium/myLitecartCountriesAndZones_2.java
@@ -0,0 +1,176 @@
+package ru.stqa.training.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+
+public class myLitecartCountriesAndZones_2 {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        //driver = new ChromeDriver();
+        //driver = new InternetExplorerDriver();
+        driver = new FirefoxDriver();
+
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest() {
+        String countryText, prevText, zone, zoneName, zoneName2, prevZoneName, prevZoneName2, countryName;
+        List <WebElement> content3, theRow, zone2, content5zone, zoneRows, zoneNames2;
+        WebElement leftPane, content1, content4, zonesLink, countryLink,zone1, zoneRow, country, zoneTable;
+        int bp, i, k, n, j, zonesNumber;
+
+        driver.get("http://localhost/litecart/admin/");
+
+        driver.findElement(By.name("username")).sendKeys("admin");      //LOGIN
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys(Keys.ENTER);
+
+        // *********************************    COUNTRIES PART :        *********************************
+
+        wait.until(presenceOfElementLocated(By.cssSelector("a[href *= countries]")));
+        //driver.get(driver.findElement(By.cssSelector("a[href *= \"countries\"]")).getAttribute("href"));
+        driver.findElement(By.cssSelector("a[href *= \"countries\"]")).click();
+        bp = 1; // just for breakpoint
+
+        content1 = driver.findElement(By.className("dataTable"));
+        content3 = content1.findElements(By.className("row"));
+        n = content3.size();
+
+        prevText = "";  // previous country text
+
+        bp = 1; // just for breakpoint
+
+        for (j = 0; j < n; j++) {                //  by Countries
+            content4 = content3.get(j);
+
+            countryLink = content4.findElement(By.cssSelector("a"));
+            countryText = countryLink.getText();
+            assertTrue(countryText.compareTo(prevText) > 0);     // by Alphabet
+            prevText = countryText;
+
+            //bp = countryText.compareTo(prevText);
+
+            theRow = content4.findElements(By.cssSelector("td"));
+            zone = theRow.get(5).getText();
+
+            if (zone.compareTo("0") > 0) {          // zone string > 0 ?
+
+                //countryLink.click();
+
+                //driver.get(countryLink.getAttribute("href"));
+                countryLink.click();
+
+                wait.until(presenceOfElementLocated(By.cssSelector("#table-zones")));
+                zone1 = driver.findElement(By.cssSelector("#table-zones"));
+                zone2 = zone1.findElements(By.cssSelector("tr"));
+                k = zone2.size();
+                prevZoneName = "";
+                for (i = 1; i < k - 1; i++) {           //  exclude 1st and last "tr" rows
+                    zoneRow = zone2.get(i);     // zone row
+                    content5zone = zoneRow.findElements(By.cssSelector("td"));
+                    zoneName = content5zone.get(2).getText();
+                    assertTrue(zoneName.compareTo(prevZoneName) > 0);   // by Alphabet
+                    prevZoneName = zoneName;
+
+                    bp = 1; // just for breakpoint
+                }
+
+                // RESTORE COUNTRIES CONTENT :
+
+                wait.until(presenceOfElementLocated(By.cssSelector("a[href *= countries]")));
+                //driver.get(driver.findElement(By.cssSelector("a[href *= \"countries\"]")).getAttribute("href"));
+                driver.findElement(By.cssSelector("a[href *= \"countries\"]")).click();
+
+                content1 = driver.findElement(By.className("dataTable"));
+                content3 = content1.findElements(By.className("row"));            //  Countries content
+
+                bp = 1; // just for breakpoint
+            } // end "if" zones presented
+        }
+
+        bp = 1; // just for breakpoint
+
+        // ********************************************* ZONES PART :   *********************************
+
+//        leftPane = driver.findElement(By.className("list-vertical"));   // Countries / Zones
+//        zonesLink = leftPane.findElement(By.cssSelector("a[href='http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones']"));
+        wait.until(presenceOfElementLocated(By.cssSelector("a[href *= 'geo_zones']")));
+        driver.get(driver.findElement(By.cssSelector("a[href *= \"geo_zones\"]")).getAttribute("href"));
+
+
+//        zonesLink.click();
+
+        content1 = driver.findElement(By.className("dataTable"));
+        content3 = content1.findElements(By.className("row"));   // all rows
+        n = content3.size();
+
+        for (j = 0; j < n; j++) {                //  by Countries
+            content4 = content3.get(j); // country row
+            zonesNumber = Integer.parseInt(content4.findElements(By.cssSelector("td")).get(3).getText());  // for debug
+            theRow = content4.findElements(By.cssSelector("a"));   // row elements
+            country = theRow.get(0);    // country link
+            countryName = country.getText();   // for debugging only
+            driver.get(country.getAttribute("href"));
+//            country.click();
+
+            bp = 1; // just for breakpoint
+
+            wait.until(presenceOfElementLocated(By.cssSelector("#table-zones")));
+            zoneTable = driver.findElement(By.cssSelector("#table-zones"));
+            //zoneTable = driver.findElement(By.cssSelector("table.dataTable"));
+            zoneRows = zoneTable.findElements(By.cssSelector("tr"));   // all rows
+            k=zoneRows.size();
+
+            prevZoneName2 = "";
+            for(i=1; i < k-1; i++){ // by country zones (exclude header and footer)
+                zoneNames2 = zoneRows.get(i).findElements(By.cssSelector("td select option[selected=selected]"));
+                zoneName2 = zoneNames2.get(1).getText();
+
+                assertTrue(zoneName2.compareTo(prevZoneName2) > 0);   // zones are by Alphabet
+                prevZoneName2 = zoneName2;
+
+                bp = 1; // just for breakpoint
+            }
+
+            // RESTORE ZONES CONTENT :
+
+//            leftPane = driver.findElement(By.className("list-vertical"));   // Countries / Zones
+//            zonesLink = leftPane.findElement(By.cssSelector("a[href='http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones']"));
+//            zonesLink.click();
+
+            wait.until(presenceOfElementLocated(By.cssSelector("a[href *= 'geo_zones']")));
+            driver.get(driver.findElement(By.cssSelector("a[href *= \"geo_zones\"]")).getAttribute("href"));
+
+
+            content1 = driver.findElement(By.className("dataTable"));
+            content3 = content1.findElements(By.className("row"));   // all rows
+
+        } // end "For" by Countries
+
+        bp = 1; // just for breakpoint
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+
+
+}
--- src/test/java/ru/stqa/training/selenium/myLitecartNewWindows.java
+++ src/test/java/ru/stqa/training/selenium/myLitecartNewWindows.java
@@ -24,6 +24,7 @@ import static java.lang.Thread.*;
 import static org.junit.Assert.assertTrue;
 import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementLocated;
 // import static org.openqa.selenium.PageLoadStrategy.EAGER;
 
 public class myLitecartNewWindows {
@@ -40,16 +41,18 @@ public class myLitecartNewWindows {
         FirefoxOptions options = new FirefoxOptions().setPageLoadStrategy(PageLoadStrategy.EAGER);
         driver = new FirefoxDriver(options);
 
-////        InternetExplorerOptions optionsIE = new InternetExplorerOptions().setPageLoadStrategy(PageLoadStrategy.EAGER);
-////        driver = new InternetExplorerDriver(optionsIE);
-
-          // EAGER, NONE, NORMAL (EAGER doesn't work ?)
+        // EAGER, NONE, NORMAL (EAGER doesn't work ?)
 //        ChromeOptions optionsChr = new ChromeOptions().setPageLoadStrategy(PageLoadStrategy.NORMAL);
 //        driver = new ChromeDriver(optionsChr);
 
-        wait = new WebDriverWait(driver, 10);
+       //  IE  DOESN'T work with windows handles correctly :
+// //       InternetExplorerOptions optionsIE = new InternetExplorerOptions().setPageLoadStrategy(PageLoadStrategy.NORMAL);
+//  //      driver = new InternetExplorerDriver(optionsIE);
 
-        // driver.manage().window().maximize();
+
+        wait = new WebDriverWait(driver, 25);
+
+        driver.manage().window().maximize();
 
         // Browser :
 //        Capabilities caps = ((RemoteWebDriver) driver).getCapabilities();
@@ -60,52 +63,69 @@ public class myLitecartNewWindows {
 
     @Test
     public void myFirstTest() {
-        String handleMain, handleNew="";
+        String handleMain, handleNew="", extLink;
         List <WebElement> linksExt;
         WebElement linkArg;
-        int bp, i, j;
+        int bp, i, j, bef, aft;
         Set<String> handlesAll;
 
         driver.get("http://localhost/litecart/admin/");
 
         driver.findElement(By.name("username")).sendKeys("admin");      //LOGIN
         driver.findElement(By.name("password")).sendKeys("admin");
-        driver.findElement(By.name("password")).sendKeys(Keys.ENTER);
+        wait.until(elementToBeClickable(By.name("login")));
+        driver.findElement(By.name("login")).click();
 
         // *********************************    COUNTRIES Page :        *********************************
 
         wait.until(presenceOfElementLocated(By.cssSelector("a[href *= countries]")));
-        driver.findElement(By.cssSelector("div#box-apps-menu-wrapper a[href $= \"doc=countries\"]")).click();     // Countries
+        driver.get(driver.findElement(By.cssSelector("div#box-apps-menu-wrapper a[href $= \"doc=countries\"]")).getAttribute("href"));
 
-        wait.until(presenceOfElementLocated(By.cssSelector("a[href $= AR]")));
-        linkArg = driver.findElement(By.cssSelector("table.dataTable tr.row a[href $= AR][title = Edit]"));
-        linkArg.click();
+        //wait.until(presenceOfElementLocated(By.cssSelector("a[href $= AR]")));
+        wait.until(elementToBeClickable(By.cssSelector("table.dataTable tr.row a[href $= AR][title = Edit]")));
+        linkArg = driver.findElement(By.cssSelector("a[title = Edit][href $= AR]"));
+        WebElement link2 =  linkArg.findElement(By.cssSelector("i.fa"));  // fa fa-pencil
+        wait.until(elementToBeClickable(link2));
+        //driver.get(linkArg.getAttribute("href"));
+        link2.click();
 
         wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("h1[style ^= \"margin-top\"]"), "Edit Country"));
-        linksExt = driver.findElements(By.cssSelector( "i[class = \"fa fa-external-link\"]"));
+        linksExt = driver.findElements(By.cssSelector( "a[target='_blank'] i[class='fa fa-external-link']"));
+        //linksExt = driver.findElements(By.xpath("//i[@class='fa fa-external-link']/.."));
         int num = linksExt.size();
 
         handleMain = driver.getWindowHandle(); // current window
         handleNew = handleMain;
 
-        for (i=0; i < num; i++) {
+        for (i=0; i < num; i++) {       // by all external links
 
-            handlesAll = driver.getWindowHandles();   // ALL handles
-            int bef = handlesAll.size();
-            int aft = 0;
+            handlesAll = driver.getWindowHandles();   // ALL windows handles
+            bef = handlesAll.size();
+            aft = 0;
 
-            linksExt = driver.findElements(By.cssSelector( "i[class = \"fa fa-external-link\"]"));
+            //linksExt = driver.findElements(By.xpath("//i[@class='fa fa-external-link']/.."));  // links external
+            //((JavascriptExecutor)driver).executeScript("window.open();");
+
+            // open new window by clicking Link :
+            linksExt = driver.findElements(By.cssSelector("a[target='_blank'] i[class='fa fa-external-link']"));
             linksExt.get(i).click();
 
-            //handleNew = wait.until(anyWindowOtherThan(handlesAll));
-
+            int kk = 1;
             do {
-                handlesAll = driver.getWindowHandles();   //  wait new window
+                handlesAll = driver.getWindowHandles();         // wait for new window !
                 aft = handlesAll.size();
-            }while (aft <= bef);
 
-            for ( String s : handlesAll ){    // cause only 2 handles exist. else method is required.
-                if ( ! (handleMain.equals(s)) ) {
+                kk++;
+                if (kk > 1000) break;
+
+            } while (aft <= bef);
+
+            bp = 1; // just for breakpoint  TESTING IE - driver.getWindowHandles() - resume : DOESN'T WORK.   (IE 32 driver tested in Windows 10 64 bit)
+            handlesAll = driver.getWindowHandles();   // opening here new window by manual click doesn't affect number of windows handles in IE only.
+            bp = 1; // just for breakpoint
+
+            for (String s : handlesAll) {    // cause only 2 handles exist. else method is required.
+                if (!(handleMain.equals(s))) {
                     handleNew = s;
                     break;
                 }
@@ -117,13 +137,32 @@ public class myLitecartNewWindows {
 //            } catch (InterruptedException ex) { ; }
 
             driver.switchTo().window(handleNew);
+
+            //wait.until(presenceOfElementLocated(By.cssSelector("html body")));  // check element on page just in case
+            // Check text on pages (Change to cycle with string array later) :
+            if (i == 0)
+                wait.until(textToBePresentInElementLocated(By.cssSelector("html body div h1"), "ISO 3166-1 alpha-2"));  // check element
+            if (i == 1)
+                wait.until(textToBePresentInElementLocated(By.cssSelector("html body div h1"), "ISO 3166-1 alpha-3"));  // check element
+            if (i == 2)
+                wait.until(textToBePresentInElementLocated(By.cssSelector("html body div h1"), "Regular expression"));  // check element
+            if (i == 3){    // works but too long in Chrome
+                wait.until(presenceOfElementLocated(By.cssSelector("#contact-us-button > div > a > span")));  // check element
+                //wait.until(textToBePresentInElementLocated(By.cssSelector("#contact-us-button > div > a > span"), "Contact us"));  // check element
+            }  // #contact-us-button > div > a > span
+            if(i==4)
+                wait.until(textToBePresentInElementLocated(By.cssSelector("html body div h1"), "Regular expression"));  // check element
+
+            if(i==5)
+                wait.until(textToBePresentInElementLocated(By.cssSelector("html body div h1"), "List of countries and capitals with currency and language"));  // check element
+            if(i==6)
+                wait.until(textToBePresentInElementLocated(By.cssSelector("html body div h1"), "List of country calling codes"));  // check element
+
             driver.close();                         // close new window
 
             driver.switchTo().window(handleMain);    // go to main window
 
         }
-
-        bp = 1; // just for breakpoint
 
     }
 
